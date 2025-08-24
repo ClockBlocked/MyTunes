230b68e66eabdf45596663cbbd371edc6674eaee
ClockBlocked <4theloveofcarl@gmail.com>
2025-08-24 10:32:45 -0500

    Delete helpers.js

--- helpers.js
+++
@@ -1,6 +0,0 @@
-
-export function create(htmlString) {
-  const template = document.createElement("template");
-  template.innerHTML = htmlString.trim();
-  return template.content.firstElementChild;
-}
